874593eba37e6d8c5e864beecd4b718288040f8d
Maulana Kevin Pradana <maulanakevinaresa@gmail.com>
2019-03-23 08:26:05 +0700

    kalkulator hewan ternak kambing
    monggo diliat

--- src/project/Project.java
+++ src/project/Project.java
@@ -15,7 +15,8 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Kalkulator k1 = new Kalkulator();
+        k1.setVisible(true);
     }
     
 }
